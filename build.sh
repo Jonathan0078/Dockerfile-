2c3d6f0148651d55e8e4e1faa244e2ee2c4cd94b
Jonathan0078 <92819780+Jonathan0078@users.noreply.github.com>
2025-06-29 23:54:31 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 # Exit on error
 set -o errexit
 
+echo "--- Iniciando o processo de build ---"
+
+echo "Instalando dependências..."
 pip install -r requirements.txt
 
+echo "Inicializando o banco de dados..."
 flask init-db
+
+echo "--- Build concluído com sucesso! ---"
